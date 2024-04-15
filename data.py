6b3598e3a758b5dd2a104c4a1ffadea8c42fdebf
Rashmi <118280805+RashK19@users.noreply.github.com>
2024-04-15 23:13:32 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import mysql.connector
+
+root=tk.Tk()
+root.geometry("500x500")
+root.resizable(width=False, height=False)
+
+l1=tk.Label(text="Name",font="14").place(x=100,y=100)
+l2=tk.Label(text="Age",font="14").place(x=100,y=200)
+
+name=
+
+e_name=tk.Entry(root,textvariable=).place
+
+root.mainloop()
